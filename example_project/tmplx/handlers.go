edb5bc33b6c34f3c73ae3f269227b09602c57986
許煜廷 <me@hsuyuting.com>
2025-07-31 19:38:15 +0800

    Update demo

--- example_project/tmplx/handlers.go
+++ example_project/tmplx/handlers.go
@@ -76,6 +76,69 @@ document.addEventListener('DOMContentLoaded', function() {
 
   <p>counter * 10 = {{.field_4}}</p>
 
+  <a href="/second-page">second page</a>
+
+
+</body></html>
+{{end}}
+{{define "/second-page"}}
+<html><head>
+
+
+
+  <title> {{.field_1}} </title>
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+  const state = JSON.parse(this.getElementById("tx-state").innerHTML)
+  const addHandler = (node) => {
+    const walker = document.createTreeWalker(
+      node,
+      NodeFilter.SHOW_ELEMENT,
+      (n) => {
+        for (let attr of n.attributes) {
+          if (attr.name.startsWith('tx-on')) {
+            return NodeFilter.FILTER_ACCEPT;
+          }
+        }
+        return NodeFilter.FILTER_SKIP
+      }
+    );
+    while (walker.nextNode()) {
+      const cn = walker.currentNode;
+      for (let attr of cn.attributes) {
+        if (attr.name.startsWith('tx-on')) {
+          const eName = attr.name.slice(5);
+          cn.addEventListener(eName, async () => {
+            const states = {}
+
+            for (let key in state) {
+              states[key] = JSON.stringify(state[key])
+            }
+            const res = await fetch("/tx/" + attr.value + "?" + new URLSearchParams(states).toString())
+            res.text().then(html => {
+              document.open()
+              document.write(html)
+              document.close()
+            })
+          })
+        }
+      }
+    }
+  }
+
+  new MutationObserver((records) => {
+    records.forEach((record) => {
+      if (record.type !== 'childList') return
+      records.addedNodes()
+    })
+  }).observe(document.documentElement, { childList: true, subList: true })
+  addHandler(document.documentElement)
+});
+</script><script type="application/json" id="tx-state">{{.state}}</script></head>
+
+<body>
+  <h1> {{.field_2}} </h1>
+
 
 </body></html>
 {{end}}
@@ -85,13 +148,13 @@ var tmplxHandlers []TmplxHandler = []TmplxHandler{
 {
 	Url: "/{$}",
 	HandlerFunc: func(w http.ResponseWriter, r *http.Request) {
-		var title string = "Tmplx!"
+		var title string = "Tmplx"
 var h1 string = "Hello, Tmplx!"
 var counter int = 0
 var counterTimes10 int = counter * 10
 counterTimes10 = counter * 10
 
-		tmpl.ExecuteTemplate(w, "/{$}", map[string]any{"field_1": title, "field_2": h1, "field_3": counter, "field_4": counterTimes10, "state": map[string]any{"title": title, "h1": h1, "counter": counter, "counterTimes10": counterTimes10}})
+		tmpl.ExecuteTemplate(w, "/{$}", map[string]any{"field_3": counter, "field_4": counterTimes10, "field_1": title, "field_2": h1, "state": map[string]any{"title": title, "h1": h1, "counter": counter, "counterTimes10": counterTimes10}})
 	},
 },
 {
@@ -108,7 +171,7 @@ var counterTimes10 int = counter * 10
 counter++
 counterTimes10 = counter * 10
 
-		tmpl.ExecuteTemplate(w, "/{$}", map[string]any{"field_1": title, "field_2": h1, "field_3": counter, "field_4": counterTimes10, "state": map[string]any{"title": title, "h1": h1, "counter": counter, "counterTimes10": counterTimes10}})
+		tmpl.ExecuteTemplate(w, "/{$}", map[string]any{"field_3": counter, "field_4": counterTimes10, "field_1": title, "field_2": h1, "state": map[string]any{"title": title, "h1": h1, "counter": counter, "counterTimes10": counterTimes10}})
 	},
 },
 {
@@ -125,7 +188,16 @@ var counterTimes10 int = counter * 10
 counter--
 counterTimes10 = counter * 10
 
-		tmpl.ExecuteTemplate(w, "/{$}", map[string]any{"field_4": counterTimes10, "field_1": title, "field_2": h1, "field_3": counter, "state": map[string]any{"title": title, "h1": h1, "counter": counter, "counterTimes10": counterTimes10}})
+		tmpl.ExecuteTemplate(w, "/{$}", map[string]any{"field_1": title, "field_2": h1, "field_3": counter, "field_4": counterTimes10, "state": map[string]any{"title": title, "h1": h1, "counter": counter, "counterTimes10": counterTimes10}})
+	},
+},
+{
+	Url: "/second-page",
+	HandlerFunc: func(w http.ResponseWriter, r *http.Request) {
+		var title string = "Second Page"
+var h1 string = "Hello, Again!"
+
+		tmpl.ExecuteTemplate(w, "/second-page", map[string]any{"field_1": title, "field_2": h1, "state": map[string]any{"title": title, "h1": h1}})
 	},
 },}
 func Handlers() []TmplxHandler { return tmplxHandlers }
