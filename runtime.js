af4daf049222b5d6431f121fa6b9af54763cd8cb
許煜廷 <me@hsuyuting.com>
2025-11-03 19:13:43 +0800

    Add tx-value

---
+++ runtime.js
@@ -0,0 +1,108 @@
+document.addEventListener('DOMContentLoaded', function() {
+  let state = JSON.parse(this.getElementById("tx-state").innerHTML)
+  let tasks = [];
+  let isProcessing = false;
+
+  const init = (cn) => {
+    for (let attr of cn.attributes) {
+      if (attr.name === 'tx-value' && cn.tagName === 'INPUT') {
+        cn.addEventListener('input', (e) => {
+          const txSwap = cn.getAttribute("tx-swap")
+          const txValue = cn.getAttribute("tx-value")
+          if (txValue) {
+            state[txSwap][txValue] = e.target.value
+            return
+          }
+        })
+      } else if (attr.name.startsWith('tx-on')) {
+        const [fun, params] = attr.value.split("?")
+        const searchParams = new URLSearchParams(params)
+        let eventName = attr.name.slice(5)
+
+        cn.addEventListener(eventName, () => {
+          tasks.push(async () => {
+            const txSwap = cn.getAttribute("tx-swap")
+            searchParams.append("tx-swap", txSwap)
+
+
+            for (let key in state) {
+              if (key.startsWith(txSwap)) {
+                searchParams.append(key, JSON.stringify(state[key]))
+              }
+            }
+
+            const res = await fetch("/tx/" + fun + "?" + searchParams.toString())
+            const html = await res.text()
+
+            if (txSwap === 'tx_') {
+              document.open()
+              document.write(html)
+              document.close()
+              return
+            }
+
+            const comp = document.createElement('body')
+            comp.innerHTML = html
+            const txState = comp.querySelector("#tx-state")
+            const newStates = JSON.parse(txState.textContent)
+            state = { ...state, ...newStates }
+            comp.removeChild(txState)
+            const range = document.createRange()
+            const start = document.getElementById(txSwap)
+            const end = document.getElementById(txSwap + '_e')
+            range.setStartBefore(start);
+            range.setEndAfter(end);
+            range.deleteContents();
+            for (let child of comp.childNodes) {
+              range.insertNode(child.cloneNode(true))
+              range.collapse(false)
+            }
+          })
+          processQueue()
+        })
+      }
+    }
+  }
+
+  async function processQueue() {
+    if (isProcessing) return;
+    isProcessing = true;
+    while (tasks.length > 0) {
+      const task = tasks.shift();
+      await task();
+    }
+    isProcessing = false;
+  }
+
+  const addHandler = (node) => {
+    if (node.nodeType === Node.TEXT_NODE) {
+      return
+    }
+
+    const walker = document.createTreeWalker(
+      node,
+      NodeFilter.SHOW_ELEMENT,
+      (n) => {
+        for (let attr of n.attributes) {
+          if (attr.name.startsWith('tx-on') || attr.name === 'tx-value') {
+            return NodeFilter.FILTER_ACCEPT;
+          }
+        }
+        return NodeFilter.FILTER_SKIP
+      }
+    );
+
+    init(walker.root)
+    while (walker.nextNode()) {
+      init(walker.currentNode)
+    }
+  }
+
+  new MutationObserver((records) => {
+    records.forEach((record) => {
+      if (record.type !== 'childList') return
+      record.addedNodes.forEach(addHandler)
+    })
+  }).observe(document.documentElement, { childList: true, subtree: true })
+  addHandler(document.documentElement)
+});
