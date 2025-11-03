6be38a0fa6d8c0e3036c7f371a1a834c837ccf9d
許煜廷 <me@hsuyuting.com>
2025-11-04 00:18:37 +0800

    Update tmplx.org

--- tmplx.org/tmplx/handler.go
+++ tmplx.org/tmplx/handler.go
@@ -6,35 +6,49 @@ import (
 	"fmt"
 	"html"
 	"io"
+	"log"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
-var runtimeScript = `
-document.addEventListener('DOMContentLoaded', function() {
+var runtimeScript = `document.addEventListener('DOMContentLoaded', function() {
   let state = JSON.parse(this.getElementById("tx-state").innerHTML)
+  let tasks = [];
+  let isProcessing = false;
 
   const init = (cn) => {
     for (let attr of cn.attributes) {
-      if (attr.name.startsWith('tx-on')) {
+      if (attr.name === 'tx-value' && cn.tagName === 'INPUT') {
+        cn.addEventListener('input', (e) => {
+          const txSwap = cn.getAttribute("tx-swap")
+          const txValue = cn.getAttribute("tx-value")
+          if (txValue) {
+            state[txSwap][txValue] = e.target.value
+            return
+          }
+        })
+      } else if (attr.name.startsWith('tx-on')) {
         const [fun, params] = attr.value.split("?")
         const searchParams = new URLSearchParams(params)
-        const eventName = attr.name.slice(5);
-        cn.addEventListener(eventName, async () => {
-          const txSwap = cn.getAttribute("tx-swap")
-          let pfx = ''
-          if (txSwap) {
-            pfx = txSwap
-          }
-          for (let key in state) {
-            if (key.startsWith(pfx)) {
-              searchParams.append(key, JSON.stringify(state[key]))
+        let eventName = attr.name.slice(5)
+
+        cn.addEventListener(eventName, () => {
+          tasks.push(async () => {
+            const txSwap = cn.getAttribute("tx-swap")
+            searchParams.append("tx-swap", txSwap)
+
+
+            for (let key in state) {
+              if (key.startsWith(txSwap)) {
+                searchParams.append(key, JSON.stringify(state[key]))
+              }
             }
-          }
-          searchParams.append("tx-swap", txSwap)
-          const res = await fetch("/tx/" + fun + "?" + searchParams.toString())
-          res.text().then(html => {
-            if (pfx === '') {
+
+            const res = await fetch("/tx/" + fun + "?" + searchParams.toString())
+            const html = await res.text()
+
+            if (txSwap === 'tx_') {
               document.open()
               document.write(html)
               document.close()
@@ -58,9 +72,20 @@ document.addEventListener('DOMContentLoaded', function() {
               range.collapse(false)
             }
           })
+          processQueue()
         })
       }
     }
+  }
+
+  async function processQueue() {
+    if (isProcessing) return;
+    isProcessing = true;
+    while (tasks.length > 0) {
+      const task = tasks.shift();
+      await task();
+    }
+    isProcessing = false;
   }
 
   const addHandler = (node) => {
@@ -73,7 +98,7 @@ document.addEventListener('DOMContentLoaded', function() {
       NodeFilter.SHOW_ELEMENT,
       (n) => {
         for (let attr of n.attributes) {
-          if (attr.name.startsWith('tx-on')) {
+          if (attr.name.startsWith('tx-on') || attr.name === 'tx-value') {
             return NodeFilter.FILTER_ACCEPT;
           }
         }
@@ -101,114 +126,68 @@ type TmplxHandler struct {
 	Url         string
 	HandlerFunc http.HandlerFunc
 }
-type state_tx_h_demo struct {
-	S_name     string `json:"name"`
-	S_greeting string `json:"greeting"`
-	S_counter  int    `json:"counter"`
+type state_tx_h_todo struct {
+	S_list []string `json:"list"`
+	S_item string   `json:"item"`
 }
 
-func render_tx_h_demo(w io.Writer, key string, states map[string]string, newStates map[string]any, name string, greeting string, counter int, addOne, addOne_swap string) {
-	w.Write([]byte(`<template id="`))
-	w.Write([]byte(fmt.Sprint(key)))
-	w.Write([]byte(`"></template> <h2>`))
-	w.Write([]byte(html.EscapeString(fmt.Sprint(greeting))))
-	w.Write([]byte(`</h2> <button tx-onclick="`))
-	w.Write([]byte(fmt.Sprint(addOne)))
-	w.Write([]byte(`"`))
-	if addOne_swap != "tx_" {
-		w.Write([]byte(` tx-swap="`))
-		w.Write([]byte(fmt.Sprint(addOne_swap)))
-		w.Write([]byte(`"`))
+func render_tx_h_todo(w io.Writer, key string, states map[string]string, newStates map[string]any, list []string, item string, add, add_swap string, remove, remove_swap string) {
+	w.Write([]byte("<template id=\""))
+	fmt.Fprint(w, key)
+	w.Write([]byte("\"></template> <label>New <input type=\"text\" tx-value=\"item\" tx-swap=\""))
+	fmt.Fprint(w, key)
+	w.Write([]byte("\"value=\""))
+	fmt.Fprint(w, item)
+	w.Write([]byte("\"/></label> <button tx-onclick=\""))
+	fmt.Fprint(w, add)
+	w.Write([]byte("\" tx-swap=\""))
+	fmt.Fprint(w, add_swap)
+	w.Write([]byte("\">Add</button> <ol> "))
+
+	for i, l := range list {
+		w.Write([]byte("<li tx-key=\"l\" tx-onclick=\""))
+		fmt.Fprint(w, remove)
+		w.Write([]byte("?i="))
+		if param, err := json.Marshal(i); err != nil {
+			log.Panic(err)
+		} else {
+			w.Write([]byte(url.QueryEscape(string(param))))
+		}
+		w.Write([]byte("\" tx-swap=\""))
+		fmt.Fprint(w, remove_swap)
+		w.Write([]byte("\"> "))
+		w.Write([]byte(html.EscapeString(fmt.Sprint(l))))
+		w.Write([]byte(" </li>"))
+
 	}
-	w.Write([]byte(`>Add 1</button> <p>count: `))
-	w.Write([]byte(html.EscapeString(fmt.Sprint(counter))))
-	w.Write([]byte(`</p> <p>count * 10 = `))
-	w.Write([]byte(html.EscapeString(fmt.Sprint(counter * 10))))
-	w.Write([]byte(`</p> <template id="`))
-	w.Write([]byte(fmt.Sprint(key + "_e")))
-	w.Write([]byte(`"></template>`))
+	w.Write([]byte(" </ol> <template id=\""))
+	fmt.Fprint(w, key+"_e")
+	w.Write([]byte("\"></template>"))
 }
 
 type state_index struct {
 }
 
 func render_index(w io.Writer, key string, states map[string]string, newStates map[string]any) {
-	w.Write([]byte(`<!DOCTYPE html><html lang="en"><head> <title>a tmplx</title> <meta charset="UTF-8"/> <meta name="viewport" content="width=device-width, initial-scale=1.0"/> <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/modern-normalize@3.0.1/modern-normalize.min.css"/> <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.11.1/build/styles/tokyo-night-dark.min.css"/> <script src="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.11.1/build/highlight.min.js"></script> <script src="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.11.1/build/languages/html.min.js"></script> <script>hljs.highlightAll();</script> <style>
-    body {
-      color: #16161d;
-      background: #efeff3;
-      display: flex;
-      font-size: 1rem;
-      line-height: 1.5rem;
-    }
-
-    main {
-      margin-left: auto;
-      margin-right: auto;
-      width: 40rem;
-    }
-
-    h1 {
-      margin-top: 8rem;
-      font-family: Verdana;
-      font-size: 4rem;
-    }
-
-    mark {
-      background: Cornsilk;
-    }
-
-    .btn {
-      background: DimGray;
-      color: white;
-      text-decoration: none;
-      padding: 0.5rem 1rem;
-      border-radius: 0.5rem;
-      font-size: 1.5rem;
-
-      &:hover {
-        background: gray;
-      }
-    }
-  </style> <script id="tx-runtime">`))
+	w.Write([]byte("<!DOCTYPE html><html lang=\"en\"><head> <title>a tmplx </title> <meta charset=\"UTF-8\"/> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"/> <link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/modern-normalize@3.0.1/modern-normalize.min.css\"/> <link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.11.1/build/styles/tokyo-night-dark.min.css\"/> <script src=\"https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.11.1/build/highlight.min.js\"></script> <script src=\"https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.11.1/build/languages/html.min.js\"></script> <script>hljs.highlightAll();</script> <style>\n    body {\n      color: #16161d;\n      background: #efeff3;\n      display: flex;\n    }\n\n    main {\n      margin: 4rem auto;\n      width: 40rem;\n    }\n\n    h1 {\n      font-family: Verdana;\n      font-size: 3rem;\n    }\n\n    h2 {\n      margin-top: 2rem;\n      margin-bottom: 1rem;\n    }\n\n    a {\n      color: RoyalBlue;\n    }\n\n    pre {\n      margin: 0;\n    }\n\n    .btn {\n      text-decoration: none;\n      border: solid;\n      border-radius: 0.25rem;\n      color: #16161d;\n      padding: 0.5rem 2rem;\n      font-weight: bold;\n      background-color: LightGray;\n\n      &:hover {\n        background-color: Silver;\n      }\n    }\n\n    li {\n      cursor: pointer;\n\n      &:hover {\n        text-decoration: line-through\n      }\n    }\n  </style> <script id=\"tx-runtime\">"))
 	w.Write([]byte(runtimeScript))
-	w.Write([]byte(`</script><script type="application/json" id="tx-state">TX_STATE_JSON</script></head> <body> <main> <h1 style="text-align:center">&lt;tmplx&gt;</h1> <p style="font-size:1.5rem;text-align:center"> <strong>Build state-driven web app with Go in HTML.</strong></p> <div style="text-align:center;padding:4rem 0;"> <a class="btn" href="https://github.com/gnituy18/tmplx">Get Started</a> </div> <pre> <code tx-ignore="" class="language-html">&lt;script type=&#34;text/tmplx&#34;&gt;
-  // name is a state
-  var name string = &#34;tmplx&#34;
-
-  // greeting is a derived
-  var greeting string = fmt.Sprintf(&#34;Hello ,%s!&#34;, name)
-
-  var counter int = 0
-
-  // addOne event handler
-  func addOne() {
-    counter++
-  }
-&lt;/script&gt;
-
-&lt;h2&gt; { greeting } &lt;/h2&gt;
-&lt;button tx-onclick=&#34;addOne()&#34;&gt;Add 1&lt;/button&gt;
-&lt;p&gt;count: { counter }&lt;/p&gt;
-&lt;p&gt;count * 10 = { counter * 10 }&lt;/p&gt;
-      </code> </pre> `))
+	w.Write([]byte("</script><script type=\"application/json\" id=\"tx-state\">TX_STATE_JSON</script></head> <body> <main> <h1 style=\"text-align:center\"> &lt;tmplx&gt; </h1> <p style=\"text-align:center\"> <strong>Build state-driven web app with Go in HTML.</strong></p> <div style=\"text-align:center;margin-top:4rem\"> <a class=\"btn\" href=\"https://github.com/gnituy18/tmplx\">Get Started </a> </div> <h2 style=\"margin-top:4rem\"> Demo: To Do App</h2> "))
 	{
-		ckey := key + "_index_tx-demo_1"
-		state := &state_tx_h_demo{}
+		ckey := key + "_index_tx-todo_1"
+		state := &state_tx_h_todo{}
 		if _, ok := states[ckey]; ok {
 			json.Unmarshal([]byte(states[ckey]), state)
 			newStates[ckey] = state
 		} else {
-			state.S_name = "tmplx"
-			state.S_counter = 0
+			state.S_list = []string{}
+			state.S_item = ""
 			newStates[ckey] = state
 		}
-		name := state.S_name
-		greeting := fmt.Sprintf("Hello ,%s!", name)
-		counter := state.S_counter
-		render_tx_h_demo(w, ckey, states, newStates, name, greeting, counter, "tx_h_demo_addOne", ckey)
+		list := state.S_list
+		item := state.S_item
+		render_tx_h_todo(w, ckey, states, newStates, list, item, "tx_h_todo_add", ckey, "tx_h_todo_remove", ckey)
 	}
-	w.Write([]byte(` </main> </body></html>`))
+	w.Write([]byte(" <h2 style=\"margin-top:2rem\"><a href=\"https://github.com/gnituy18/tmplx/blob/main/tmplx.org/todo.html\">Source Code:</a></h2> <pre> <code tx-ignore=\"\" class=\"language-html\">&lt;script type=&#34;text/tmplx&#34;&gt;\n        var list []string = []string{}\n        var item string = &#34;&#34;\n\n        func add() {\n        list = append(list, item)\n        item = &#34;&#34;\n        }\n\n        func remove(i int) {\n        list = append(list[0:i], list[i+1:]...)\n        }\n        &lt;/script&gt;\n\n        &lt;label&gt;New &lt;input type=&#34;text&#34; tx-value=&#34;item&#34;&gt;&lt;/label&gt;\n        &lt;button tx-onclick=&#34;add()&#34;&gt;Add&lt;/button&gt;\n        &lt;ol&gt;\n        &lt;li tx-for=&#34;i, l := range list&#34; tx-key=&#34;l&#34; tx-onclick=&#34;remove(i)&#34;&gt;\n        { l }\n        &lt;/li&gt;\n        &lt;/ol&gt;</code> </pre> <p>It allows you to build UIs with React-like reactivity purely in Go.</p> <h2 style=\"margin-top:2rem\"><a href=\"https://github.com/gnituy18/tmplx/blob/main/tmplx.org/tmplx/handler.html\">Compiled Code:</a></h2> </main> </body></html>"))
 }
 
 var tmplxHandlers []TmplxHandler = []TmplxHandler{
@@ -225,7 +204,7 @@ var tmplxHandlers []TmplxHandler = []TmplxHandler{
 		},
 	},
 	{
-		Url: "/tx/tx_h_demo_addOne",
+		Url: "/tx/tx_h_todo_add",
 		HandlerFunc: func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Content-Type", "text/html")
 			query := r.URL.Query()
@@ -237,18 +216,48 @@ var tmplxHandlers []TmplxHandler = []TmplxHandler{
 				}
 			}
 			newStates := map[string]any{}
-			state := &state_tx_h_demo{}
+			state := &state_tx_h_todo{}
 			json.Unmarshal([]byte(states[txSwap]), &state)
-			name := state.S_name
-			greeting := fmt.Sprintf("Hello ,%s!", name)
-			counter := state.S_counter
-			counter++
-			greeting = fmt.Sprintf("Hello ,%s!", name)
-			render_tx_h_demo(w, txSwap, states, newStates, name, greeting, counter, "tx_h_demo_addOne", txSwap)
+			list := state.S_list
+			item := state.S_item
+			list = append(list, item)
+			item = ""
+			render_tx_h_todo(w, txSwap, states, newStates, list, item, "tx_h_todo_add", txSwap, "tx_h_todo_remove", txSwap)
 			w.Write([]byte("<script id=\"tx-state\" type=\"application/json\">"))
-			newStates[txSwap] = &state_tx_h_demo{
-				S_name:    name,
-				S_counter: counter,
+			newStates[txSwap] = &state_tx_h_todo{
+				S_list: list,
+				S_item: item,
+			}
+			stateBytes, _ := json.Marshal(newStates)
+			w.Write(stateBytes)
+			w.Write([]byte("</script>"))
+		},
+	},
+	{
+		Url: "/tx/tx_h_todo_remove",
+		HandlerFunc: func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "text/html")
+			query := r.URL.Query()
+			txSwap := query.Get("tx-swap")
+			states := map[string]string{}
+			for k, v := range query {
+				if strings.HasPrefix(k, txSwap) {
+					states[k] = v[0]
+				}
+			}
+			newStates := map[string]any{}
+			state := &state_tx_h_todo{}
+			json.Unmarshal([]byte(states[txSwap]), &state)
+			list := state.S_list
+			item := state.S_item
+			var i int
+			json.Unmarshal([]byte(query.Get("i")), &i)
+			list = append(list[0:i], list[i+1:]...)
+			render_tx_h_todo(w, txSwap, states, newStates, list, item, "tx_h_todo_add", txSwap, "tx_h_todo_remove", txSwap)
+			w.Write([]byte("<script id=\"tx-state\" type=\"application/json\">"))
+			newStates[txSwap] = &state_tx_h_todo{
+				S_list: list,
+				S_item: item,
 			}
 			stateBytes, _ := json.Marshal(newStates)
 			w.Write(stateBytes)
