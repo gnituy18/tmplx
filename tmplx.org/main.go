b09f58202529ea71c9e5d76caa511cc077f6ce50
許煜廷 <me@hsuyuting.com>
2025-11-04 13:21:37 +0800

    Update tmplx.org

--- tmplx.org/main.go
+++ tmplx.org/main.go
@@ -3,14 +3,26 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"tmplx.org/tmplx"
 )
 
 func main() {
+	env := os.Getenv("ENV")
+	cert := os.Getenv("CERT")
+	pk := os.Getenv("PK")
+
 	for _, th := range tmplx.Handlers() {
 		http.HandleFunc("GET "+th.Url, th.HandlerFunc)
 	}
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	if env == "prod" {
+		go http.ListenAndServe(":80", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			http.Redirect(w, r, "https://"+r.Host+r.URL.String(), http.StatusMovedPermanently)
+		}))
+		log.Fatal(http.ListenAndServeTLS(":443", cert, pk, nil))
+	} else {
+		log.Fatal(http.ListenAndServe(":8080", nil))
+	}
 }
