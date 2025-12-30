3dbdc5542f10508935492a69aef2d177e57c0326
許煜廷 <me@hsuyuting.com>
2025-12-31 01:00:01 +0900

    Update tmplx.org

--- example_project/go.mod
+++
@@ -1,3 +0,0 @@
-module example_project
-
-go 1.24.4
--- tmplx.org/main.go
+++ tmplx.org/main.go
@@ -13,10 +13,12 @@ func main() {
 	cert := os.Getenv("CERT")
 	pk := os.Getenv("PK")
 
-	for _, th := range tmplx.Handlers() {
-		http.HandleFunc("GET "+th.Url, th.HandlerFunc)
+	for _, th := range tmplx.Routes() {
+		http.HandleFunc(th.Pattern, th.Handler)
 	}
 
+	fs := http.FileServer(http.Dir("./assets"))
+	http.Handle("/", http.StripPrefix("/", fs))
 	if env == "prod" {
 		go http.ListenAndServe(":80", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			http.Redirect(w, r, "https://"+r.Host+r.URL.String(), http.StatusMovedPermanently)
